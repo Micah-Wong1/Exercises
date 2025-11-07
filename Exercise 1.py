689067e00e9f1f226fb00b2f6bb753d9fcc080e7
Micah-Wong1 <Wongm@educ.college-montreal.qc.ca>
2025-11-07 16:18:35 -0500

    Tirrreeeeedddddddd

---
+++ Exercise 1.py
@@ -0,0 +1,15 @@
+"""
+Nom: Micah Wong
+Gr: 406
+Ce code est la premiere exercise
+"""
+
+
+class Stringfoo:
+    def __init__(self, set_string):
+        self.message = str(set_string)
+
+
+string_1 = Stringfoo("hi")
+
+print(string_1.message)
