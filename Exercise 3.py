e39c5b067754a3e9ad92059cbd48b2130f0a902c
wongm <wongm@educ.college-montreal.qc.ca>
2026-01-13 10:45:13 -0500

    Enum and ALignments imma chaotic neutral :)

--- Exercise 3.py
+++ Exercise 3.py
@@ -1,7 +1,143 @@
 from enum import Enum
+from dataclasses import dataclass
+import random
 
 """
 Nom: Micah Wong
 Gr: 406
 Ce code est la troisieme exercise de TP4
 """
+
+
+def base_stat():
+    dice_1 = random.randint(1, 6)
+    dice_2 = random.randint(1, 6)
+    dice_3 = random.randint(1, 6)
+    dice_4 = random.randint(1, 6)
+
+    die = [dice_1, dice_2, dice_3, dice_4]
+    smallest_dice = die[0]
+
+    for i in die:
+        if i < smallest_dice:
+            smallest_dice = i
+
+    stat = dice_1 + dice_2 + dice_3 + dice_4 - smallest_dice
+    return stat
+
+
+class Alignments(Enum):
+    lawful_good = 1
+    lawful_neutral = 2
+    lawful_evil = 3
+    neutral_good = 4
+    true_neutral = 5
+    neutral_evil = 6
+    chaotic_good = 7
+    chaotic_neutral = 8
+    chaotic_evil = 9
+    undefined = 10
+
+
+@dataclass
+class Item:
+
+    nom_item = ""
+    quantite_item = 
+
+
+class NPC:
+
+    def __init__(self):
+        self.Force = base_stat()
+        self.Agility = base_stat()
+        self.Constitution = base_stat()
+        self.Intelligence = base_stat()
+        self.Sagesse = base_stat()
+        self.Charisme = base_stat()
+        self.Alignment = Alignments(random.randint(1, 10))
+        self.Armure = random.randint(1, 12)
+        self.Nom = ""
+        self.Race = ""
+        self.Espece = ""
+        self.HP = 20
+        self.Profession = ""
+
+    def afficher_caracteristiques(self):
+        print(f"Nom = {self.Nom}")
+        print(f"Espece = {self.Espece}")
+        print(f"Race = {self.Race}")
+        print(f"Profession = {self.Profession}")
+        print(f"HP = {self.HP}")
+        print(f"Force = {self.Force}")
+        print(f"Agilité = {self.Agility}")
+        print(f"Constitution = {self.Constitution}")
+        print(f"Integlligence = {self.Intelligence}")
+        print(f"Sagesse = {self.Sagesse}")
+        print(f"Charisme = {self.Charisme}")
+        print(f"Armure = {self.Armure}")
+
+
+class Kobold(NPC):
+
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def attaquer(cible):
+        power = random.randint(1, 20)
+        if power == 1:
+            pass
+            print("Attaque raté...")
+        elif power == 20:
+            cible.subir_dommages(8)
+            print("ATTAQUE CRITIQUE!!")
+        else:
+            if power >= cible.Armure:
+                cible.subir_dommages(6)
+                print("Attaque normal!")
+            else:
+                pass
+                print("Attaque raté...")
+        print(f"Health left: {cible.HP}")
+
+    def subir_dommages(self, dmg):
+        self.HP = self.HP - dmg
+
+    def vivant(self):
+        if self.HP > 0:
+            print(f"Health: {self.HP}")
+        elif self.HP <= 0:
+            print("Health: 0")
+
+
+class Hero(NPC):
+    def __init__(self):
+        super().__init__()
+
+    @staticmethod
+    def attaquer(cible):
+        power = random.randint(1, 20)
+        if power == 1:
+            pass
+            print("Attaque raté...")
+        elif power == 20:
+            cible.subir_dommages(8)
+            print("ATTAQUE CRITIQUE!!")
+        else:
+            if power >= cible.Armure:
+                cible.subir_dommages(6)
+                print("Attaque normal!")
+            else:
+                pass
+                print("Attaque raté...")
+        print(f"Health left: {cible.HP}")
+
+    def vivant(self):
+        if self.HP > 0:
+            print(f"Health: {self.HP}")
+        elif self.HP <= 0:
+            print("Health: 0")
+
+    def subir_dommages(self, dmg):
+        self.HP = self.HP - dmg
